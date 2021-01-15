fc89f7f13e1e420e66def03fce1763d6a85ede8c
z3ox1s <66672234+z3ox1s@users.noreply.github.com>
2021-01-15 07:25:40 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name = 'zget-z3ox1s',
+    version='0.0.1',
+    author='z3ox1s',
+    description = 'A tool to made HTTP Requests.',
+    url = 'https://github.com/z3ox1s/zget',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires = '>=3.0'
+)
