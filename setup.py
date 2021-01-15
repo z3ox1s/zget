9f4f78fe1a5bbe6c0ed324c01d9db2836aa8aec8
z3ox1s <66672234+z3ox1s@users.noreply.github.com>
2021-01-15 07:46:23 -0300

    Added version 0.0.1

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+from os import system
+
+setuptools.setup(
+    name = 'zget-z3ox1s',
+    version='0.0.1',
+    author='z3ox1s',
+    description = 'A tool to made HTTP Requests.',
+    url = 'https://github.com/z3ox1s/zget',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires = '>=3.6'
+)
+
+system('chmod +x zget.py ; cp zget.py ./zget ; dos2unix zget ; mv zget /usr/bin/')
